99a4fdfd9b4262069b585cb11540741c0cdffa8c
Roman Wiseman <roman.wiseman@student.manchester.ac.uk>
2022-12-30 22:31:29 +0000

    diasbled button when appropriate

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Header() {
 
-	const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } = useMoralis();
+	const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } = useMoralis();
 
 	useEffect( () => {
 		Moralis.onAccountChanged( (account) => {
@@ -29,12 +29,15 @@ export default function Header() {
 		
 		{account 
 		? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length-4)}</div>) 
-		: (<button onClick={async() => {
-			await enableWeb3();
-			if (typeof window !== undefined){
-				window.localStorage.setItem("connected", "injected");
-			}
-		}}>Connect</button>)}
+		: (
+			<button onClick={async() => {
+				await enableWeb3();
+				if (typeof window !== undefined) {
+					window.localStorage.setItem("connected", "injected");}
+			}}
+			disabled = {isWeb3EnableLoading}
+			>Connect</button>
+		)}
 
 	</div>)
 
